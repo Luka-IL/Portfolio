3e999a6a74b5b33aafc1f699e700af921d30d6ad
Ilya <dert_94@mail.ru>
2020-06-23 21:45:25 +0300

    Бургер меню, skills hover, портфолио js, плавный скролл по якорям

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(".navigation").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
+    });
+    
+    $("#btn").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('data-set'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
+    });
+    
+    $('#checkbox1').on("click", function(){
+        $('#navigation').toggleClass('display_flex');
+        $('body').toggleClass('overflow_hidden')
+    })
+    $('.navigation__link').on('click', function() {
+        $('#navigation').removeClass('display_flex');
+        $('body').removeClass('overflow_hidden')
+    })
+
+})
